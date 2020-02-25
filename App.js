bbd3bfd466f8ef116485bbec918bf64fe9828007
the last <zealotkid711@gmail.com>
2020-02-25 20:55:22 +0700

    add empty image screen

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import { createStackNavigator } from 'react-navigation-stack';
 import HomeScreen from "./src/screens/HomeScreen";
 import ComponentsScreen from './src/screens/ComponentsScreen';
 import FlatListComponent from './src/screens/FlatListComponent';
+import ImageScreen from './src/screens/ImageScreen';
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
     Components: ComponentsScreen,
-    List: FlatListComponent
+    List: FlatListComponent,
+    Image: ImageScreen
   },
   {
     initialRouteName: "Home",
